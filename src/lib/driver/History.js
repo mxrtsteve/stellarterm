b224a7c0f9cedefebd3ac2056d3d822a59a5f1ad
little-wave <little-wave@lobstr.co>
2019-04-15 13:39:55 +0300

    History refactored

--- src/lib/driver/History.js
+++ src/lib/driver/History.js
@@ -1,32 +1,31 @@
-import _ from 'lodash';
-
 import MagicSpoon from '../MagicSpoon';
 import Event from '../Event';
 
 export default function History(driver) {
-  this.event = new Event();
+    this.event = new Event();
+    this.spoonHistory = null;
+    let isLoading = false;
 
-  let active = false;
-  this.spoonHistory = null;
+    // The way this history thing is that once the user touches the history tab,
+    // they have permanently initiated history loading mode (and this is when
+    // we start to eat through all the horizon history).
 
-  // The way this history thing is that once the user touches the history tab,
-  // they have permanently initiated history loading mode (and this is when
-  // we start to eat through all the horizon history).
+    // It's simple and reliable. this.handlers.touch is idempotent so it's fine
+    // if it gets called many times
 
-  // It's simple and reliable. this.handlers.touch is idempotent so it's fine
-  // if it gets called many times
+    this.handlers = {
+        touch: () => {
+            if (isLoading === false) {
+                isLoading = true;
 
-  this.handlers = {
-    touch: (e) => {
-      if (active == false) {
-        active = true;
-        setTimeout(async () => {
-          this.spoonHistory = await MagicSpoon.History(driver.Server, driver.session.account.account_id, () => {
-            this.event.trigger();
-          });
-        }, 10);
-      }
-    },
-  };
+                setTimeout(async () => {
+                    this.spoonHistory = await MagicSpoon.History(
+                        driver.Server,
+                        driver.session.account.account_id,
+                        () => { this.event.trigger(); },
+                    );
+                }, 10);
+            }
+        },
+    };
 }
-
